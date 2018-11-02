d1d7ad6d00b577fb3f7c662f16ef8658bb7bc0db
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-11-02 14:58:54 +0100

    add ecs:ListContainerInstance to policy for cluster istanaces so they can check if they have joined the cluster

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -41,6 +41,7 @@ data "aws_iam_policy_document" "permissions" {
       "ecs:CreateCluster",
       "ecs:DeregisterContainerInstance",
       "ecs:DiscoverPollEndpoint",
+      "ecs:ListContainerInstances",
       "ecs:Poll",
       "ecs:RegisterContainerInstance",
       "ecs:StartTelemetrySession",
